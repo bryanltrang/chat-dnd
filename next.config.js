e3249d96e83007d8c4a7cab650f845392b8d0448
bryan trang <bryanltrang@gmail.com>
2023-10-24 20:43:05 -0700

    updated next config image domains

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["oaidalleapiprodscus.blob.core.windows.net", "elated-fennec-743.convex.cloud"],
+    domains: ["oaidalleapiprodscus.blob.core.windows.net", "elated-fennec-743.convex.cloud", "https://coordinated-retriever-516.convex.cloud/api/storage/f8cffb31-c6c6-49f7-b217-2818640295fe"],
   },
 }
 
